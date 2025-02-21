570af44cdaf8dcfc10f817d4e1040404410ac788
solmouse <sjkim.judy@gmail.com>
2025-02-22 02:11:10 +0900

    4차-3

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,20 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "docs"), // 배포용 폴더
-    publicPath: "/summer_school/", // GitHub Pages용 publicPath 설정 (github의 url)
+    publicPath: "/summer_school/", // GitHub Pages용 publicPath 설정
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public/data.json", to: "docs/data.json" }], // ✅ data.json 복사
+    }),
+  ],
   optimization: {
     minimize: true, // 코드 최적화
   },
